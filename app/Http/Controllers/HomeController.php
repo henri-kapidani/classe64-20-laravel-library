956dd764c35cd66e3cfe083f652ed850ac5e8db6
henri-kapidani <h.kapidani@gmail.com>
2022-07-15 12:36:36 +0400

    add more queries

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,11 +21,25 @@ class HomeController extends Controller
     }
 
     public function list() {
-        // $books = Book::all(); // tutte le colonne
-        // $books = Book::all(['title', 'price']); // limita le colonne
-        $books = Book::where('price', '>', 50)->get(['title', 'price']);
-        // dd($books);
+        $books = Book::all(); // tutte le colonne
+		dump($books); // stampa i dati come oggetto collection
+        $books->dump(); // stampa i dati come array
 
-        return view('list', compact('books'));
+        $books = Book::all(['title', 'price']); // limita le colonne
+		  dump($books);
+        $books = Book::where('price', '>', 50)->get(['title', 'price']);
+        dump($books);
+
+        // Restituisce un solo libro, non una collection di libri!
+        $book = Book::where('price', '>', 50)->first(['title', 'price']);
+        dump($book);
+        // $book->id; $book->title
+
+        $books = Book::where('price', '<', 50)->orderBy('price', 'asc')->get();
+        dump($books);
+        $books = Book::where('price', '<', 50)->orderBy('price', 'asc')->limit(1)->get();
+        dump($books);
+
+        // return view('list', compact('books'));
     }
 }
